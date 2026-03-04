d2c0b5d26e16fc3b41978b76e98bac4a14bc846b
cmdrumright <cdrumright@protonmail.com>
2026-03-04 15:23:53 -0600

    - Changed classes to accept constructor arguments - Changed instance calls to pass in arguments

--- animals.py
+++ animals.py
@@ -8,199 +8,169 @@ from datetime import date
 
 # define walking classes
 class Llama:
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 class Donkey:
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 class Goat:
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 class Camel:
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 class Sheep:
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 # define slithering classes
 class Snake:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 class Lizard:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 class Newt:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 class Iguana:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 class Frog:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 # define swimming classes
 class Goldfish:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 class Bass:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 class Salmon:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 class Koi:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 class Catfish:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 # create instances of walking classes
-miss_fuzz = Llama()
-miss_fuzz.name = "Miss Fuzz"
-miss_fuzz.species = "domestic llama"
+miss_fuzz = Llama("Miss Fuzz", "domestic llama")
 
-archibald = Donkey()
-archibald.name = "Archibald"
-archibald.species = "Ass"
+archibald = Donkey("Archibald", "Ass")
 
-billy = Goat()
-billy.name = "Billy"
-billy.species = "fainting goat"
+billy = Goat("Billy", "fainting goat")
 
-twin_peaks = Camel()
-twin_peaks.name = "Twin Peaks"
-twin_peaks.species = "Saharan Camel"
+twin_peaks = Camel("Twin Peaks", "Saharan Camel")
 
-fluffy = Sheep()
-fluffy.name = "Fluffy"
-fluffy.species = "Highland Sheep"
+fluffy = Sheep("Fluffy", "Highland Sheep")
 
 # create instances of slithering classes
-arthur = Snake()
-arthur.name = "Arthur"
-arthur.species = "king snake"
+arthur = Snake("Arthur", "king snake")
 
-sir_john = Lizard()
-sir_john.name = "Sir John"
-sir_john.species = "spotted rock hopper"
+sir_john = Lizard("Sir John", "spotted rock hopper")
 
-issac_newton = Newt()
-issac_newton.name = "Issac Newton"
-issac_newton.species = "science newt"
+issac_newton = Newt("Issac Newton", "science newt")
 
-iggy = Iguana()
-iggy.name = "Iggy"
-iggy.species = "Australian green iguana"
+iggy = Iguana("Iggy", "Australian green iguana")
 
-kermit = Frog()
-kermit.name = "Kermit"
-kermit.species = "puppet frog"
+kermit = Frog("Kermit", "puppet frog")
 
 # create instances of swimming classes
-popeye = Goldfish()
-popeye.name = "Popeye"
-popeye.species = "popeye goldfish"
+popeye = Goldfish("Popeye", "popeye goldfish")
 
-gary = Bass()
-gary.name = "Gary"
-gary.species = "Large mouth bass"
+gary = Bass("Gary", "Large mouth bass")
 
-hopper = Salmon()
-hopper.name = "Hopper"
-hopper.species = "red salmon"
+hopper = Salmon("Hopper", "red salmon")
 
-spot = Koi()
-spot.name = "Spot"
-spot.species = "orange koi"
+spot = Koi("Spot", "orange koi")
 
-whiskers = Catfish()
-whiskers.name = "Whiskers"
-whiskers.species = "Tennessee catfish"
+whiskers = Catfish("Whiskers", "Tennessee catfish")
 
 # print walker objects
 print("Petting Area:")
